0a6865049abbb2f8e860c56cf36a4c213868e2dc
kenmugy <kenmugy@gmail.com>
2019-08-31 10:12:42 +0300

    added get route to get all books and take any query

--- app.js
+++ app.js
@@ -5,14 +5,24 @@ const morgan = require('morgan');
 const chalk = require('chalk');
 
 const app = express();
+// open database connection
+const db = mongoose.connect('mongodb://localhost/cityAPI', {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
 const port = process.env.PORT || 8000;
 const cityRouter = express.Router();
+const City = require('./models/cityModel');
 
-cityRouter.route('/').get((req, res) => {
-  const reply = {
-    message: 'hello welcome to my api'
-  };
-  res.json(reply);
+cityRouter.route('/cities').get((req, res) => {
+  const query = {};
+  if (req.query.rank) {
+    query.rank = req.query.rank;
+  }
+  City.find(query, (err, cities) => {
+    if (err) return res.send(err);
+    return res.json(cities);
+  });
 });
 app.use(morgan('dev'));
 app.use('/api', cityRouter);
